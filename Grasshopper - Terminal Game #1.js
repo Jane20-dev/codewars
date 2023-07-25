75fde0f45c7cc4d6ec0d37943a4f2429e1880f31
Jane <evgenia.kacarskaya@mail.ru>
2023-07-25 19:56:45 +0300

    my new message

---
+++ Grasshopper - Terminal Game #1.js
@@ -0,0 +1,11 @@
+function Hero (name) {
+  
+    this.name = name || 'Hero';
+    this.experience = 0;
+    this.health = 100;
+    this.position = '00';
+    this.damage = 5;
+    
+    return name
+    
+  }
